75ea3329b4a7fa987b1aa581a350921f77fee960
ofcdougls <ofc.dougls@outlook.com>
2023-04-30 19:18:57 -0300

    atividade concluida

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Cryptography;
+
+Console.WriteLine("--- Rolagem de Dado ---\n");
+
+Console.Write("Quantas faces? ");
+int faces = Convert.ToInt32(Console.ReadLine());
+
+int rolagem = RandomNumberGenerator.GetInt32(1, faces + 1);
+
+Console.WriteLine($"\nRolagem: {rolagem}");
